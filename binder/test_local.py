7eebca70fe1062e2ff7df67ec5c03a86ddf88854
Kitty <kathryn.harris@ucdenver.edu>
2022-04-26 15:30:22 -0600

    Switch to library designed to run notebook directly. Hoping this will work better for coverage

--- binder/test_local.py
+++ binder/test_local.py
@@ -1,25 +1,22 @@
 import subprocess
 import tempfile
 import os
+from pytest_notebook.execution import execute_notebook
+from pytest_notebook.notebook import load_notebook
 
-def _exec_notebook(path):
-    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
-        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-                "--ExecutePreprocessor.timeout=10000",
-                "--ExecutePreprocessor.kernel_name=python3",
-                "--output", fout.name, path]
-        subprocess.check_call(args)
-
+n = [0,0,0,0,0,0,0,0,0,0]
+n[0] = load_notebook('01_DM_Rotation_Curve_Intro.ipynb')
+n[1] = load_notebook('02_Widget_NGC5533_DMonly.ipynb')
+n[2] = load_notebook('03_Measured_Data_Plotting.ipynb')
+n[3] = load_notebook('04_Plotting_Rotation_Curves.ipynb')
+n[4] = load_notebook('05_Widget_NGC5533_All_Components.ipynb')
+n[5] = load_notebook('06_Plotting_SPARC_data.ipynb')
+n[6] = load_notebook('07_Bonus_Bulge_Rotation_Curve.ipynb')
+n[7] = load_notebook('08_Interactive_Fitting.ipynb')
+n[8] = load_notebook('09_Widget_SPARC_Galaxies.ipynb')
+n[9] = load_notebook('10_Bonus_Black_Holes_as_DM.ipynb')
 
 def test():
     print('Testing Jupyter notebooks...')
-    _exec_notebook('01_DM_Rotation_Curve_Intro.ipynb')
-    _exec_notebook('02_Widget_NGC5533_DMonly.ipynb')
-    _exec_notebook('03_Measured_Data_Plotting.ipynb')
-    _exec_notebook('04_Plotting_Rotation_Curves.ipynb')
-    _exec_notebook('05_Widget_NGC5533_All_Components.ipynb')
-    _exec_notebook('06_Plotting_SPARC_data.ipynb')
-    _exec_notebook('07_Bonus_Bulge_Rotation_Curve.ipynb')
-    _exec_notebook('08_Interactive_Fitting.ipynb')
-    _exec_notebook('09_Widget_SPARC_Galaxies.ipynb')
-    _exec_notebook('10_Bonus_Black_Holes_as_DM.ipynb')
+    for notebook in n:
+        execute_notebook(notebook,with_coverage=True)
